805339d206ff10a5f537437727ceadbefa5f094f
Easton Tuttle <eastontuttuhl@gmail.com>
2021-03-16 16:02:34 -0600

    Embedded messages and cleanup

--- bot.js
+++ bot.js
@@ -7,50 +7,62 @@ const { prefix } = require('./config.json');
 client.on('message', receivedMessage);
 
 function receivedMessage(message) {
-    if(message.content === `${prefix}pick`) {
+    if (message.content === `${prefix}pick`) {
         const hardpointMaps = ['Moscow', 'Raid', 'Checkmate', 'Crossroads', 'Garrison'],
-        searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Garrison'],
-        controlMaps = ['Raid', 'Checkmate', 'Garrison'],
-        members = message.member.voice.channel.members;
-
+            searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Garrison'],
+            controlMaps = ['Raid', 'Checkmate', 'Garrison'],
+            members = message.member.voice.channel.members;
         let teams = randomizeTeams(createPlayerList(members));
-        console.log('teams: ' + teams);
-        console.log('team1: ' + teams[0]);
-        console.log('team2: ' + teams[1]);
-        
-        message.channel.send(respondWithTeams(teams) + respondWithMaps(hardpointMaps, searchMaps, controlMaps));
-    } else if(message.content === `${prefix}caps`) {
-        const hardpointMaps = ['Moscow', 'Raid', 'Checkmate', 'Crossroads', 'Garrison'],
-        searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Garrison'],
-        controlMaps = ['Raid', 'Checkmate', 'Garrison'],
-        members = message.member.voice.channel.members;
+        const messageEmbed = new Discord.MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle('Randomized Teams/Maps')
+            .setDescription('Good luck to both teams and enjoy the match!')
+            .addField('Team 1', teams[0], true)
+            .addField('Team 2', teams[1], true)
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
+            .addField('Control', selectMap(controlMaps), true)
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
 
+        message.channel.send(messageEmbed);
+    } else if (message.content === `${prefix}caps`) {
+        const hardpointMaps = ['Moscow', 'Raid', 'Checkmate', 'Crossroads', 'Garrison'],
+            searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Garrison'],
+            controlMaps = ['Raid', 'Checkmate', 'Garrison'],
+            members = message.member.voice.channel.members;
         let captains = randomizeCaptains(createPlayerList(members));
 
-        message.channel.send(respondWithCaptains(captains) + respondWithMaps(hardpointMaps, searchMaps, controlMaps));
-    } else if(message.content === `${prefix}maps`) {
-        const hardpointMaps = ['Moscow', 'Raid', 'Checkmate', 'Crossroads', 'Garrison'],
-        searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Garrison'],
-        controlMaps = ['Raid', 'Checkmate', 'Garrison'];
+        const messageEmbed = new Discord.MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle('Randomized Captains/Maps')
+            .setDescription('Good luck to both teams and enjoy the match!')
+            .addField('First Captain', captains[0], true)
+            .addField('Second Captain', captains[1], true)
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
+            .addField('Control', selectMap(controlMaps), true)
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
 
-        message.channel.send(respondWithMaps(hardpointMaps, searchMaps, controlMaps));
+        message.channel.send(messageEmbed);
+    } else if (message.content === `${prefix}maps`) {
+        const hardpointMaps = ['Moscow', 'Raid', 'Checkmate', 'Apocalypse', 'Garrison'],
+            searchMaps = ['Moscow', 'Raid', 'Checkmate', 'Miami', 'Express'],
+            controlMaps = ['Raid', 'Checkmate', 'Garrison'];
+
+        const messageEmbed = new Discord.MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle('Randomized Maps')
+            .setDescription('Good luck to both teams and enjoy the match!')
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
+            .addField('Control', selectMap(controlMaps), true)
+            .addField('Hardpoint', selectMap(hardpointMaps), true)
+            .addField('Search and Destroy', selectMap(searchMaps), true)
+
+        message.channel.send(messageEmbed);
     }
-}
-
-function respondWithMaps(hardpointMaps, searchMaps, controlMaps) {
-    return '\n' + selectMap(hardpointMaps) + ' | ' + 'Hardpoint \n'
-    + selectMap(searchMaps) + ' | ' + 'Search and Destroy \n'
-    + selectMap(controlMaps) + ' | '  + 'Control \n'
-    + selectMap(hardpointMaps) + ' | ' + 'Hardpoint \n'
-    + selectMap(searchMaps) + ' | ' + 'Search and Destroy \n'
-}
-
-function respondWithTeams(teams) {
-    return '\n' + 'Team 1: ' + teams[0] + '\nTeam 2: ' + teams[1];
-}
-
-function respondWithCaptains(captains) {
-    return '\n' + 'First Pick Captain: ' + captains[0] + '\nSecond Pick Captain: ' + captains[1];
 }
 
 function selectMap(maps) {
@@ -61,7 +73,7 @@ function createPlayerList(members) {
     let players = [];
 
     members.forEach(element => players.push(element.user.username));
-    
+
     return players;
 }
 
@@ -69,13 +81,13 @@ function randomizeTeams(players) {
     let teamOne = [],
         teamTwo = [];
 
-    for(var i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
         let randNum = Math.floor(Math.random() * players.length);
         teamOne.push(players[randNum]);
         players.splice(randNum, 1);
     }
 
-    for(var i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
         let randNum = Math.floor(Math.random() * players.length);
         teamTwo.push(players[randNum]);
         players.splice(randNum, 1);
@@ -91,16 +103,16 @@ function randomizeCaptains(players) {
         captainTwo = '',
         randNumOne = Math.floor(Math.random() * players.length),
         randNumTwo = Math.floor(Math.random() * players.length);
-    
-        captainOne = players[randNumOne];
-        players.splice(randNumOne, 1);
 
-        captainTwo = players[randNumTwo];
-        players.splice(randNumTwo, 1);
+    captainOne = players[randNumOne];
+    players.splice(randNumOne, 1);
 
-        let captains = [captainOne, captainTwo];
+    captainTwo = players[randNumTwo];
+    players.splice(randNumTwo, 1);
 
-        return captains;
+    let captains = [captainOne, captainTwo];
+
+    return captains;
 }
 
 client.login(process.env.TOKEN);
